fcdd0be2e75ef2aac18221114923c5b8a67fc43d
Akram <akramhoussam02@hotmail.com>
2020-05-20 19:54:30 +0200

    Creación de la clase Factura
    Hecho por AlumnoB (Akram)

---
+++ Facturas/src/Kraken/Factura.java
@@ -0,0 +1,13 @@
+package Kraken;
+
+public class Factura {
+	public int num;
+	public float base;
+	public float tipoIva;
+	public Estados estado; // Aun no esta implementado el enumerado Estados.
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
