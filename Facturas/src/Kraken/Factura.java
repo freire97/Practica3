9dbccee02500e2e81d968d1be7cd360941a65812
Cris <crisnavas10@gmail.com>
2020-05-20 23:51:10 +0200

    Métodos implementados

--- Facturas/src/Kraken/Factura.java
+++ Facturas/src/Kraken/Factura.java
@@ -7,7 +7,8 @@ private class Factura implements Ifactura {
 	protected Estados estado;
 	
 	public static void main(String[] args) {
-
+		getTotal();
+		getCantidadIva();
 	}
 
 	public getTotal() {
