eaec276847d7be7de10531b6798e7c146461ce5b
Akram <akramhoussam02@hotmail.com>
2020-05-20 20:14:16 +0200

    Linea descomentada
    Hecho por AlumnoB(Cambios) Akram

--- Facturas/src/Kraken/Factura.java
+++ Facturas/src/Kraken/Factura.java
@@ -4,7 +4,7 @@ public class Factura {
 	public int num;
 	public float base;
 	public float tipoIva;
-	public Estados estado; // Aun no esta implementado el enumerado Estados.
+	public Estados estado;
 	
 	public static void main(String[] args) {
 		
